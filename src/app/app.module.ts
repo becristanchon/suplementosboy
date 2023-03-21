56069202d18e6e504b9cf9e6bfe92be3851c7705
becristanchon <42919456+becristanchon@users.noreply.github.com>
2023-03-21 12:09:50 -0500

    linea base del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    CartComponent,
+    CheckoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'suplementosboy-b4e1b',
+    appId: '1:821402729922:web:6b797ce80a9414972daa5b',
+    storageBucket: 'suplementosboy-b4e1b.appspot.com',
+    apiKey: 'AIzaSyB4PC00u4-vk3BA2MbGu6ClHNuQdn9U6_s',
+    authDomain: 'suplementosboy-b4e1b.firebaseapp.com',
+    messagingSenderId: '821402729922',
+    measurementId: 'G-S630L1VREQ',
+  },};
